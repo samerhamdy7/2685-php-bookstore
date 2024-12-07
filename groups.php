c473d227c594d27d250684bc62dba49cd6c4a873
Samer Hamdy <samerhamdyemam@gmail.com>
2024-12-08 00:25:43 +0200

    finish

--- groups.php
+++ groups.php
@@ -3,74 +3,97 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>2685</title>
    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
 
-    $group_2685 = ['Maged Yassen' , 'Nour' , 'Salma El-Qady' , 'Hager Essam' , 'Ahmed Khaled' , 'Nada Ahmed' ,'Mahmoued Sahwan' , 'Samer Hamdy' , 'Adam Sherif', 'Youssif Elsayed' , 'Amr Safy' , 'Mayar Ali' ,'Maya Younes' , 'Mahmoued Nader', 'Zeyad Dewedar' , 'Ahmed Sayed' , 'Ahmed Khalid' , 'Alyaa' , 'Ahmed Wallied' , 'Karim Fawzy'];
+<?php 
 
-    /*$check_name = ''; 
-    if(in_array($check_name, $group_2685)) {
-        echo 'true';
-
-    }else {
-        echo 'false';
-    };*/
+$group_2685 = [
+    ["name" => "Maged Yaseen", "role" => "INSTRUCTOR <br/> Full Stack Web Developer" , "img" => "maged.png" ],
+    ["name" => "Nour ", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Salma El-Qady", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Hager Essam", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Ahmed Khaled", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Nada Ahmed", "role" => "Back-End Developer", "img" => "back developer.png"],
+    ["name" => "Mohamed Sahwan", "role" => "Back-End Developer", "img" => "back developer.png"],
+    ["name" => "Samer Hamdy", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Adam Sherif", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Youssif Elsayed", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Amr Safy", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Mayar Ali", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Maya Younes", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Mohamed Nader", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Zeyad Dewedar", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Ahmed Sayed", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Ahmed Khalid", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Alyaa ", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Ahmed Wallied", "role" => "Back-End Developer" , "img" => "back developer.png"],
+    ["name" => "Karim Fawzy", "role" => "Back-End Developer" , "img" => "back developer.png"],
+];
 
  
+   /*function check_name($name, $array){
+    foreach($array as $a){
+     if(strtolower($a['name']) == strtolower($name)){
+      return true;
+     }
+    } return false;
+   };
 
-    $f_c = [];
+   $name_check = 'samer hamdy';
+   if(check_name($name_check , $group_2685)){
+    echo "<span class='true_name'>$name_check is in Array</span>";
+   }else {echo "$name_check is not Array";}*/
 
+   
 
-    foreach($group_2685 as $g){
+    function getinit($name) {
 
-        $names = explode(' ', $g);
+    $f_l = substr($name , 0 , 1);
 
-        $FL = strtoupper($names[0][0]);
+    $name_part = explode(' ', $name);
+    
+    $l_name = end($name_part);
+    
+    $s_l = substr($l_name , 0 , 1);
 
-        $lname = end($names);
+    return $f_l . $s_l;
 
-        $SL = strtoupper($lname[0][0]);
-
-        $init = $FL . $SL;
-
-        $f_c[] = $init;
-
-    };
-
-    foreach($f_c as $i){
-        echo " <div class = 'container'>
-        <div class='icon-image'>
-        <div class='icon'>
-          <span>$i </span>
-        </div>
-        <div class='hover-image'>
-          <div class='img'>
-            <span>$i</span>
-          </div>
-          <div class='content'>
-            <div class='details'>
-              <div class='name'>$i</div>
-              <div class='local_name'></div>
-              <div></div>
-              <div><strong></strong></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>";
     }
+    
+    $instructor = array_shift($group_2685);
+    $student = $group_2685;
 
-
-
- 
-   
-   
-   
+           
+    $instructor_init = getinit($instructor['name']);
+    
+    echo "
+    <div class='container'> 
+       <div class='card instructor'>
+       <img src='{$instructor['img']}' />
+       <div class='initials'>{$instructor_init}</div>
+       <h2>{$instructor['name']}</h2>
+       <p>{$instructor['role']}</p>
+    </div>";
     
     
+    echo "
+    <div class='students-container'>"; 
+    foreach ($student as $st)
+    { 
+      $student_init = getinit($st['name']);
+      echo 
+      "<div class='card student'> 
+      <img src='{$st['img']}' />
+      <div class='initials'>{$student_init} <span>2685</span></div>
+      <p>{$st['name']}</p>
+      <p>{$st['role']}</p>
+      </div>";}
+
+     echo "</div>";
+     
     ?>
 </body>
 </html>
